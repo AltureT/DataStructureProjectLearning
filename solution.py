4148ee66dfdfd50512a3c0d5bec518a3c840a09c
alturet <myalture@gmail.com>
2022-02-22 22:35:30 +0800

    链表查找完善

--- solution.py
+++ solution.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-import global_val
 from model import *
 
 
 def array_find(data: list, name: str) -> list:
-    '''
+    """
     从数组形式存储的学生数据中查找学生信息
     :param data: 数组方式存储的学生
     :param name: 需要查询的姓名
     :return: 返回列表[姓名:str，邮箱:str，联系方式:str]
-    '''
+    """
 
     return []
 
--- solutiondev.py
+++ solutiondev.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 import re
 
-import model
 from model import *
 
 
-def array_find(data: list, name: str) -> model.User:
+def array_find(data: list, name: str) -> User:
     """
-    从数组形式存储的学生数据中查找学生信息
-    :param data: 数组方式存储的学生:[User(1),User(2),User(3)....]
-    :param name: 需要查询的姓名
-    :return: 返回用户对象,若不存在返回 None
-    格式：return i
+    目的：从数组形式存储的用户数据中根据用户姓名查找信息，data数组已经根据姓名做升序排序。
+    :param data: 数组方式存储的用户:[User(1),User(2),User(3)....]，获取data[0]用户姓名的方式（str类型）：data[0].get_user_name()
+    :param name: 待查找对象的姓名
+    :return: 返回用户对象 User,若不存在返回 None
     """
     for i in data:
         if i.get_user_name() == name:
@@ -20,23 +18,52 @@ def array_find(data: list, name: str) -> model.User:
 
 
 def link_find(root: LinkNode, name):
-    pass
+    """
+    目的：从链表中根据用户姓名查找用户信息,链表已经根据姓名升序
+    节点模型：
+    class LinkNode:
+        def __init__(self, val: User):
+            self.val = val
+            self.next = None
+    :param root:链表的表头,获取当前节点姓名的方式：root.val.get_user_name()
+    :param name:待查找对象的姓名
+    :return:返回用户对象 User,若不存在返回 None
+    """
+    cur = root
+    while cur:
+        if cur.val.get_user_name() == name:
+            return cur.val
+        cur = cur.next
+    return None
 
 
 def tree_find(root: TreeNode, name):
+    """
+    目的：从二叉树中根据用户姓名查找用户信息,树已经根据姓名排序，保证左子树姓名小于右子树
+    节点模型：
+    class TreeNode:
+        def __init__(self, val: User):
+            self.val = val
+            self.left = None
+            self.right = None
+    :param root:树的的根节点,获取当前节点姓名的方式：root.val.get_user_name()
+    :param name:待查找对象的姓名
+    :return:返回用户对象 User,若不存在返回 None
+    """
+
     pass
 
 
-def array_add(data: list, u: model.User) -> list:
+def array_add(data: list, u: User) -> list:
     data.append(u)
     return data
 
 
-def link_add(root: model.LinkNode, u: model.User):
+def link_add(root: LinkNode, u: User):
     pass
 
 
-def tree_add(root: model.TreeNode, u: model.User):
+def tree_add(root: TreeNode, u: User):
     pass
 
 
@@ -52,7 +79,7 @@ def check_tel(tel) -> bool:
         return False
     elif tel[0] != "1":
         return False
-    elif tel[1:2] != "3" and tel[1:3] != "5" and tel[1:3] != "7" and tel[1:3] != "8":
+    elif tel[1:2] != "3" and tel[1:2] != "5" and tel[1:2] != "7" and tel[1:2] != "8":
         return False
     for i in range(2, 11):
         if tel[i] < "0" or tel[i] > "9":
