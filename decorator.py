8c5182b9269868eb014577919b81ebfcf44bb8ce
alturet <myalture@gmail.com>
2022-03-06 22:58:10 +0800

    细节修改

--- decorator.py
+++ decorator.py
@@ -1,10 +1,6 @@
 import functools
-import os
-import sys
 import threading
 from time import time
-
-import psutil
 
 import global_val
 
@@ -27,31 +23,3 @@ def runtime(func):
         return result
 
     return wrapper
-
-
-def trace_malloc(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        # tracemalloc.start()
-        # snapshot1 = tracemalloc.take_snapshot()
-        # result = func(*args, **kwargs)
-        # snapshot2 = tracemalloc.take_snapshot()
-        result = func(*args, **kwargs)
-        print(str(sys.getsizeof(result) / 1024) + 'KB')
-        return result
-
-    return wrapper
-
-
-def count_info(func):
-    def float_info(*args, **kwargs):
-        pid = os.getpid()
-        p = psutil.Process(pid)
-        info_start = p.memory_full_info().uss
-        result = func(*args, **kwargs)
-        info_end = p.memory_full_info().uss
-        print("程序占用了内存" + str(info_end - info_start) + "B")
-
-        return result
-
-    return float_info
